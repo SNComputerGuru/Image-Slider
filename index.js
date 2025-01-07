2d323c6b1f49eaaa672a78dfd56fefa996831ac0
SNComputerGuru <shillingndivho4@gmail.com>
2025-01-07 21:22:33 +0200

    image

---
+++ index.js
@@ -0,0 +1,38 @@
+const imageUrls = [
+    "./slider0.jpg",
+    "./slider1.jpg",
+    "./slider2.jpg",
+    "./slider3.jpg",
+    "./slider4.jpg"
+  ];
+
+var imageContainer = document.getElementById("image-container");
+var nextButton = document.getElementById("next-button");
+var previousButton = document.getElementById("previous-button");
+
+var currentIndex = 1;
+var length = imageUrls.length;
+imageContainer.src = imageUrls[currentIndex];
+
+nextButton.addEventListener("click", function() {
+    currentIndex++;
+    if(currentIndex>length){
+        currentIndex=0;
+        imageContainer.src = imageUrls[currentIndex];
+    }
+    else{
+        imageContainer.src = imageUrls[currentIndex];
+    }
+});
+
+previousButton.addEventListener("click", function(){
+    currentIndex--;
+
+    if(currentIndex<0){
+        currentIndex = length-1;
+        imageContainer.src = imageUrls[currentIndex];
+    }
+    else{
+        imageContainer.src = imageUrls[currentIndex];
+    }
+})
